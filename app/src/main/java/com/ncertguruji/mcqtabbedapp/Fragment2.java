6ff4fe7b18bfa4eedd2077d55b5d9f794b88dbd9
Divyam Kumar <divyamkumarsingh@gmail.com>
2021-03-18 12:07:55 +0530

    implemented mcq with check button

--- app/src/main/java/com/ncertguruji/mcqtabbedapp/Fragment2.java
+++ app/src/main/java/com/ncertguruji/mcqtabbedapp/Fragment2.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,15 +14,33 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment2 extends Fragment {
+
+    RadioGroup radioGroup;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment2_layout,container,false);
         Button button = view.findViewById(R.id.nfButton);
+        Button buttonCheck = view.findViewById(R.id.check);
+        radioGroup = view.findViewById(R.id.skyColor);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity().getApplicationContext(), "Hey there", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (radioGroup.getCheckedRadioButtonId()==R.id.blue){
+                    Toast.makeText(getActivity().getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+                    RadioButton correctRadioButton = view.findViewById(R.id.blue);
+                    correctRadioButton.setBackgroundColor(getResources().getColor(R.color.green));
+                }
+                else {
+                    Toast.makeText(getActivity().getApplicationContext(), "Wrong", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         return view;
